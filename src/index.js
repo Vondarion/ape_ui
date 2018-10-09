9c3bfa2cb5ac0b0f159b93cacf2eea266691a13a
fgruber <info@florian-gruber.de>
2018-10-09 08:14:34 +0200

    init

--- src/index.js
+++ src/index.js
@@ -2,35 +2,106 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Ape extends React.Component {
-  render() {
-    return (
-      <div className="ape">
-        <div className="ape-chat">
-          <Chat />
-        </div>
-        <div className="ape-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+
 
 class Chat extends React.Component {
-  renderMessage(message) {
-    return <ChatMessage />
+  constructor(props) {
+    super(props);
+    this.state = { messages: [], text: '' };
+    this.handleChangeChatMessageText = this.handleChangeChatMessageText.bind(this);
+    this.handleSubmitChatMessage = this.handleSubmitChatMessage.bind(this);
   }
+  // constructor(props) {
+  //   super(props);
+  //   this.messages = [{sender:"Florian", text:"Hallo!"}, {sender:"Katrin", text:"Hallo!"}]
+  //   this.message = {
+  //     text: "write something"
+  //   }
+  //   //Array(9).fill(null),
+  // }
+
+  // renderMessage(message) {
+  //   return <ChatMessage />
+  // }
 
   render() {
     return (
       <div className="chat">
-        <div className="log">
-          {this.renderMessage(message)}
+        <div>
+          <ChatMessageList messages={this.chat.messages}/>
         </div>
-        <div className="text">
-        <button className="send">{/* TODO */}</button></div>
+       <div>
+         <ChatMessageInput />
+       </div>
+      </div>
+    );
+  }
+
+  handleSubmitChatMessage(e) {
+    e.preventDefault();
+    if (!this.message.text.length) {
+      return;
+    }
+    const newMessage = {
+      text: this.message.text,
+      sender: "Ape",
+      id: Date.now()
+    };
+
+    this.addMessage(message => ({
+      items: message.items.concat(newMessage),
+      text: ''
+    }));
+  }
+
+  handleChangeChatMessageText(e) {
+    this.setState({ text: e.target.value });
+  }
+}
+
+class ChatMessageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chat = { messages: [], text: '' };
+    this.handleChangeChatMessageText = this.handleChangeChatMessageText.bind(this);
+    this.handleSubmitChatMessage = this.handleSubmitChatMessage.bind(this);
+  }
+}
+
+class ChatMessageInput extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmitChatMessage}>
+        <label htmlFor="new-chat-message">
+          Tell it as it is
+        </label>
+        <input
+          id="new-chat-message"
+          onChange={this.handleChangeChatMessageText}
+          value={this.message.text}
+        />
+        <button>
+          Send
+        </button>
+      </form>
+    );
+  }
+}
+
+class ChatMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.message = {
+      sender:"Florian", 
+      text:"Hallo!",
+    }
+  }
+
+  render() {
+    return (
+      <div className="message">
+      <div className="sender">Florian</div>
+      <div className="text">Guten morgen</div>
       </div>
     );
   }
@@ -43,7 +114,7 @@ class Chat extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Chat />,
+  <Ape />,
   document.getElementById('root')
 );
   
